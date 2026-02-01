ffa0ba8e3c74db534e24a60d3f2a4bff3fc081aa
roman3857 <romanshakhrai6@gmail.com>
2026-02-01 15:41:30 +0100

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+const email = 'romanshakhrai033@gmail.com';
+console.log(email.includes('@'));
+console.log(email.length);
+
+
+const My = "My";
+const name = "name";
+const is = "is";
+const messege = My + " " + name + " " + is + " Victor";
+console.log(messege)
+
+const userName = prompt("Як вас звати?")
+const messeg = alert("Дякуємо " + userName + "!" + " До сплати 300 гривень");
